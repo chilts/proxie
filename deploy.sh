02334c55c109f3f02606450f9092d21c9c7ed428
Andrew Chilton <andychilton@gmail.com>
2013-05-09 21:43:26 +1200

    Add deploy script and upstart config

---
+++ deploy.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+## ----------------------------------------------------------------------------
+
+set -e
+
+## ----------------------------------------------------------------------------
+# Set these to your preferred values.
+
+source env.sh
+
+echo "Checking all env vars have been set ..."
+if [ -z "$PROXIMITY_USER" ]; then
+    echo "Please set the PROXIMITY_USER env var"
+    exit 2
+fi
+if [ -z "$PROXIMITY_GROUP" ]; then
+    echo "Please set the PROXIMITY_GROUP env var"
+    exit 2
+fi
+if [ -z "$PROXIMITY_NODE" ]; then
+    echo "Please set the PROXIMITY_NODE env var"
+    exit 2
+fi
+echo
+
+## ----------------------------------------------------------------------------
+# Firstly, make sure that proximity can listen on port 80.
+
+# http://www.debian-administration.org/article/Running_network_services_as_a_non-root_user
+echo "Setting up authbind to allow $PROXIMITY_USER:$PROXIMITY_GROUP to use port 80 ..."
+sudo touch /etc/authbind/byport/80
+sudo chown $PROXIMITY_USER:$PROXIMITY_GROUP /etc/authbind/byport/80
+sudo chmod 755 /etc/authbind/byport/80
+echo
+
+## ----------------------------------------------------------------------------
+
+echo "Fetching new code ..."
+git pull --rebase
+echo
+
+## ----------------------------------------------------------------------------
+
+echo "Installing new npm packages ..."
+npm install
+echo
+
+## ----------------------------------------------------------------------------
+
+echo "Setting up various directories ..."
+sudo mkdir -p /var/log/proximity/
+sudo chown $PROXIMITY_USER:$PROXIMITY_GROUP /var/log/proximity/
+echo
+
+## ----------------------------------------------------------------------------
+
+# add the upstart scripts
+echo "Copying upstart scripts ..."
+m4 \
+    -D __USER__=$PROXIMITY_USER \
+    -D __NODE__=$PROXIMITY_NODE \
+    -D __PROJ__=$PROXIMITY_PROJ \
+    -D __DIRS__="$PROXIMITY_DIRS" \
+    etc/init/proximity.conf.m4 | sudo tee /etc/init/proximity.conf
+echo
+
+## ----------------------------------------------------------------------------
+
+# restart proximity
+echo "Restarting services ..."
+sudo service proximity restart
+echo
+
+## --------------------------------------------------------------------------------------------------------------------
---
+++ etc/init/proximity.conf.m4
@@ -0,0 +1,37 @@
+## ----------------------------------------------------------------------------
+#
+# Upstart script for proximity.
+#
+## ----------------------------------------------------------------------------
+
+# info
+description "proximity - simple configurable http proxy based on bouncy."
+author      "Andrew Chilton"
+
+# respawn this task
+start on runlevel [2345]
+respawn
+respawn limit 20 5
+stop on shutdown
+
+# allow opening of more than 1024 files
+limit nofile 4096 4096
+
+# set some environment variables
+env NODE_ENV=production
+
+# the script itself
+script
+
+    # quit the script if something goes wrong
+    set -e
+
+    # run the webserver as ubuntu
+    exec \
+        sudo -E -u __USER__ \
+        __NODE__ \
+        __PROJ__/proximity.js 80 __DIRS__ 2>&1 >> /var/log/proximity/proxy.log
+
+end script
+
+## ----------------------------------------------------------------------------
