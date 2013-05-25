9c45e7271a83fad46ce745f34eb7f8ba95a32211
Andrew Chilton <andychilton@gmail.com>
2013-05-25 15:51:11 +1200

    Turn proximity into a pluggable server with different strategies

--- proximity.js
+++ proximity.js
@@ -10,7 +10,6 @@ var fs = require('fs');
 var url = require('url');
 var http = require('http');
 
-var httpProxy = require('http-proxy');
 var iniparser = require('iniparser');
 var log2 = require('log2');
 var send = require('send');
@@ -23,19 +22,31 @@ process.title = 'proximity';
 var cfgFile = '/etc/proximity.ini';
 var cfgDir  = '/etc/proximity.d';
 var cfg     = iniparser.parseSync(cfgFile);
-var port    = cfg.port;
+var port    = process.argv[2] || cfg.port;
 var logfile = cfg.logfile;
-var log;
-if ( logfile === 'stdout' ) {
-    log = log2({ stream : process.stdout });
-}
-else {
-    log = log2({ stream : fs.createWriteStream(logfile) }); // ToDo: make this append
-}
+var stream  = logfile === 'stdout' ? process.stdout : fs.createWriteStream(logfile);
+var log     = log2({ stream : stream });
 
 line();
 log('Started');
 
+// ----------------------------------------------------------------------------
+// strategies and sites
+
+var strategy = {};
+var strategies = [
+    'load-balancer-round-robin',
+    'not-found',
+    'proxy',
+    'redirect',
+    'static',
+];
+
+strategies.forEach(function(name) {
+    strategy[name] = require('./lib/' + name + '.js');
+});
+
+// sites
 var sites = {};
 
 // ----------------------------------------------------------------------------
@@ -82,72 +93,24 @@ files.forEach(function(proxyfile) {
 
 log('Sites : ' + JSON.stringify(sites));
 
-var proxy = new httpProxy.RoutingProxy();
-
 var server = http.createServer(function (req, res) {
-    var host = (req.headers.host || '').replace(/:\d+$/, '');
+    var hostname = (req.headers.host || '').replace(/:\d+$/, '');
 
     // if there is no host, then 404
-    if ( !sites[host] ) {
-        log('Unknown host = ' + host);
-        res.statusCode = 404;
-        res.write('404 - Not Found\r\n');
-        return res.end();
+    if ( !sites[hostname] ) {
+        return strategy['not-found'](hostname, undefined, req, res, log);
     }
 
     // get hold of this site
-    var site = sites[host];
+    var site = sites[hostname];
 
-    // The type of site will be one of the following:
-    // * redirect
-    // * static
-    // * proxy
-
-    // see if this request should be a redirect
-    if ( site.type === 'redirect' ) {
-        log(req.headers.host + ' -> ' + site.to + req.url);
-        res.statusCode = 301;
-        res.setHeader('Location', site.to + req.url);
-        res.write('See ' + site.to + req.url + '\r\n');
-        return res.end();
+    // if there is a strategy for this site.type, send it there
+    if ( strategy[site.type] ){
+        return strategy[site.type](hostname, site, req, res, log);
     }
 
-    // if this is a static site, serve using ...
-    if ( site.type === 'static' ) {
-        var path = url.parse(req.url).pathname;
-        log(req.headers.host + ' :: ' + site.dir + path);
-
-        send(req, path)
-            .root(site.dir)
-            .on('error', function(err) {
-                res.statusCode = err.status || 500;
-                res.end(err.code === 'ENOENT' ? '404 - Not Found\r\n' : err.message);
-            })
-            .on('file', function(path, stat) {
-                log('wanting a file');
-            })
-            .on('directory', function() {
-                res.statusCode = 301;
-                res.setHeader('Location', req.url + '/');
-                res.end('Redirecting to ' + req.url + '/');
-            })
-            .pipe(res)
-        ;
-        return;
-    }
-
-    // bounce to this server
-    if ( site.type === 'proxy' ) {
-        log(host + ' == ' + site.host + ':' + site.port + req.url);
-        proxy.proxyRequest(req, res, { host : site.host, port : site.port });
-        return;
-    }
-
-    // we don't know what type of site this is, so just serve a 404
-    log('Unknown site type = ' + site.type);
-    res.statusCode = 404;
-    res.write('404 - Not Found\r\n');
-    return res.end();
+    // else, we don't know what type this site is, so we'll use the Not Found strategy
+    strategy['not-found'](hostname, site, req, res, log);
 });
 
 server.listen(port, function() {
